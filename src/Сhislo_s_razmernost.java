a5861bc8ab4d0e73f35167fd1fd2ae5dd4143fcd
Igorlo <9440533@gmail.com>
2017-03-06 19:26:24 +0300

    Добавил основные методы: ->Умножение ->Деление (как частный случай умножения) ->Сравнение

--- src/Сhislo_s_razmernost.java
+++ src/Сhislo_s_razmernost.java
@@ -1,13 +1,17 @@
- public class Сhislo_s_razmernost {
+import java.util.ArrayList;
+
+public class Сhislo_s_razmernost {
 
     private int mant;
     private int pokaz;
-    private final String razmer;
+    private  ArrayList<String> chisl;
+    private  ArrayList<String> znam;
 
-    public Сhislo_s_razmernost(int mant, int pokaz, String razmer) {
+    public Сhislo_s_razmernost(int mant, int pokaz, ArrayList<String> chisl, ArrayList<String> znam ) {
         this.mant = mant;     // Мантиса числа
         this.pokaz = pokaz;   // Показатель степени
-        this.razmer = razmer; // Размерность
+        this.chisl = chisl;   // Числитель размерности
+        this.znam = znam;     // Знаменатель размерности
     }
     public int getMant(){
         return mant;
@@ -15,8 +19,11 @@
     public int getPokaz(){
         return pokaz;
     }
-    public String getRazmer(){
-        return razmer;
+    public ArrayList<String> getZnam(){
+        return znam;
+    }
+    public ArrayList<String> getChisl(){
+        return chisl;
     }
      public void setMant(int mant){
          this.mant = mant;
@@ -31,42 +38,87 @@
         }
         return result;
     }
+    private Сhislo_s_razmernost peren(Сhislo_s_razmernost m){ // Перенос десятки из мантисы в показатель числа
+            int count = 0;
+            int newMant = m.mant;
+        while (newMant % 10 == 0 ){
+            count ++;
+            newMant /= 10;
+        }
+        return new Сhislo_s_razmernost(newMant, pokaz + count, this.chisl, this.znam );
+    }
 
 
-    public Сhislo_s_razmernost slozh(Сhislo_s_razmernost other) throws IllegalArgumentException {
-        if (razmer == other.razmer) {
-            Сhislo_s_razmernost result = new Сhislo_s_razmernost(mant, pokaz, razmer);
-            if (pokaz == other.pokaz)
-                result = new Сhislo_s_razmernost(mant + other.mant, pokaz, razmer);
+    private Сhislo_s_razmernost sokr (Сhislo_s_razmernost n){ // Сокращение числителя и знаменателя в размерности
+        if (n.chisl.isEmpty() || n.znam.isEmpty()){
+            return this;
+        }
+        else {
+            for (String ellemChisl: chisl){
+                for (String ellemZnam: znam){
+                    if (ellemChisl.equals(ellemZnam)){
+                        n.chisl.remove(ellemChisl);
+                        n.znam.remove(ellemZnam);
+                    }
+                }
+            }
+        }
+        return this;
+    }
+
+
+
+    public Сhislo_s_razmernost slozh(Сhislo_s_razmernost other) throws IllegalArgumentException { //Сложение
+        if ((chisl == other.chisl) && (znam == other.znam)) {
+            Сhislo_s_razmernost result = new Сhislo_s_razmernost(mant, pokaz, chisl, znam);
+            if (pokaz == other.pokaz) {
+                result = new Сhislo_s_razmernost(mant + other.mant, pokaz, chisl, znam);
+                return peren(result);
+            }
             if (pokaz > other.pokaz) {
-                result = new Сhislo_s_razmernost(mant * stepen(10,pokaz - other.pokaz) + other.mant, other.pokaz, razmer);
+                result = new Сhislo_s_razmernost(mant * stepen(10,pokaz - other.pokaz) + other.mant, other.pokaz, chisl, znam);
+                return peren(result);
             }
             if (pokaz < other.pokaz) {
-                result = new Сhislo_s_razmernost(other.mant * stepen(10,other.pokaz - pokaz) + mant, pokaz, razmer);
+                result = new Сhislo_s_razmernost(other.mant * stepen(10,other.pokaz - pokaz) + mant, pokaz, chisl, znam);
+                return peren(result);
             }
-            while (result.mant % 10 == 0){
-                result.setPokaz(result.pokaz + 1);
-                result.setMant(result.mant / 10);
-            }
+
         }
         throw new IllegalArgumentException();
     }
 
 
 
-     public Сhislo_s_razmernost vich(Сhislo_s_razmernost other) throws IllegalArgumentException {
-         return this.slozh(new Сhislo_s_razmernost(-other.mant, pokaz, razmer));
+     public Сhislo_s_razmernost vich(Сhislo_s_razmernost other) throws IllegalArgumentException { //Вычетание
+         return this.slozh(new Сhislo_s_razmernost(-other.mant, pokaz, chisl, znam));
      }
-     public Сhislo_s_razmernost ymn(Сhislo_s_razmernost other) throws IllegalArgumentException {
-         if
 
 
+     public Сhislo_s_razmernost ymn(Сhislo_s_razmernost other) {  //Умножение
+         ArrayList<String> newChisl = new ArrayList<String>();
+         newChisl.addAll(chisl);
+         newChisl.addAll(other.chisl);
+         ArrayList<String> newZnam = new ArrayList<String>();
+         newZnam.addAll(znam);
+         newZnam.addAll(other.znam);
+         Сhislo_s_razmernost result = new Сhislo_s_razmernost(mant * other.mant, pokaz + other.pokaz, newChisl, newZnam);
+         return peren(sokr(result));
+     }
 
 
-         while (result.mant % 10 == 0){
-             result.setPokaz(result.pokaz + 1);
-             result.setMant(result.mant / 10);
+     public Сhislo_s_razmernost del(Сhislo_s_razmernost other) { // Деление
+         return this.ymn(new Сhislo_s_razmernost(other.mant, -other.pokaz, other.chisl, other.znam ));
+     }
+
+     public String sravn(Сhislo_s_razmernost other) {
+         Сhislo_s_razmernost k = this.vich(other);
+         if (k.mant < 0){
+             return  "Правое число больше";
          }
+         if (k.mant > 0){
+             return  "Левое число больше";
+         }
+         else return "Числа равны";
      }
-
 }
